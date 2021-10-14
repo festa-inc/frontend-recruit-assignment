d1ee8821099a78f0e6991c8813fb0eb726e7c98e
Kevin Jin <tost8295@gmail.com>
2021-10-14 12:49:05 +0900

    Add StoryBook

---
+++ components/Datepicker/DatePicker.stories.tsx
@@ -0,0 +1,17 @@
+import { Story, Meta } from '@storybook/react'
+import React, { FC } from 'react'
+import DatePicker, { DatePickerProps } from './DatePicker'
+
+export default {
+  title: 'Molecules/DatePicker',
+  component: DatePicker,
+} as Meta
+
+const Template: Story<DatePickerProps> = (args) => {
+  return <DatePicker {...args} />
+}
+
+export const DatePickerDefault = Template.bind({})
+DatePickerDefault.args = {
+  value: new Date(),
+}
---
+++ components/Datepicker/DatePicker.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+export interface DatePickerProps {}
+
+const DatePicker: FC<DatePickerProps> = () => {
+  return <div />
+}
+
+export default DatePicker
